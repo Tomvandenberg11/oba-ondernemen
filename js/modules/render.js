6c4c48ce090846160dfc7d7a697b645417558e83
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-03-14 14:25:23 +0100

    styling and modules

---
+++ js/modules/render.js
@@ -0,0 +1,18 @@
+export function render(data) {
+  const results = data.results
+
+  results.forEach((item, i) => {
+    const html = `
+                <article>
+                  <h2>${item.titles[0]}</h2>
+                  <p>${
+                    item.summaries ? item.summaries[0] : "Geen samenvatting"
+                  }</p>
+                  <img src="${
+                    item.coverimages ? item.coverimages[1] : "Geen samenvatting"
+                  }">
+                </article>
+              `
+    container.insertAdjacentHTML("afterend", html)
+  })
+}
