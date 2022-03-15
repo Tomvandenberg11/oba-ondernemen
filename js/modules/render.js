f5b9bff855b8a59e4ff7e706d704b5a8de90cfed
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-03-15 12:00:28 +0100

    search

--- js/modules/render.js
+++ js/modules/render.js
@@ -3,16 +3,13 @@ export function render(data) {
 
   const results = data.results
 
-  results.forEach((item, i) => {
+  results.forEach((item) => {
     const html = `
                  <article>
                    <h2>${item.titles[0]}</h2>
                    <p>${
                      item.summaries ? item.summaries[0] : "Geen samenvatting"
                    }</p>
-                   <img src="${
-                     item.coverimages ? item.coverimages[1] : "Geen foto"
-                   }">
                  </article>
                `
     container.insertAdjacentHTML("afterbegin", html)
