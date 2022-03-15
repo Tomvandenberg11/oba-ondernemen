1a33d7d8595108cb24a5a2416f3598f2ad929d50
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-03-15 13:07:03 +0100

    boek

--- js/modules/render.js
+++ js/modules/render.js
@@ -4,14 +4,25 @@ export function render(data) {
   const results = data.results
 
   results.forEach((item) => {
+    console.log(item)
+
     const html = `
                  <article>
-                   <h2>${item.titles[0]}</h2>
-                   <p>${
-                     item.summaries ? item.summaries[0] : "Geen samenvatting"
-                   }</p>
+                  <div>
+                    <h2>${item.titles[0]}</h2>
+                    <p>${
+                      item.summaries
+                        ? item.summaries[0].length > 150
+                          ? item.summaries[0].substring(0, 150) + ".."
+                          : item.summaries[0]
+                        : "Geen samenvatting"
+                    }</p>
+                  </div>
+                  <img width="50%" src="${item.coverimages[1]}"/>
                  </article>
                `
-    container.insertAdjacentHTML("afterbegin", html)
+    if (item.formats[0].text === "Boek") {
+      container.insertAdjacentHTML("afterbegin", html)
+    }
   })
 }
